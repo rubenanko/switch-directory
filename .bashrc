a50c42bec52517b3fbc74ce8a394f9ef73ccf287
rubenanko <rubenanko@gmail.com>
2025-07-10 22:49:18 +0200

    feat(): set shortcut to current directory, and default .bashrc

---
+++ .bashrc
@@ -0,0 +1,5 @@
+for i in {0..9}
+do
+    alias "s${i}"="sd $i ."
+    alias "${i}"="sd $i"
+done
--- sd.c
+++ sd.c
@@ -1,32 +1,48 @@
-#include <sys/ioctl.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+#ifdef __linux__
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <linux/limits.h>
+#endif
 
 
 #define TEMPLATE "SWITCH_"
 
 void inject_shell(const char* cmd){
-  int i = 0;
-  while (cmd[i] != '\0'){
-    ioctl(0, TIOCSTI, &cmd[i++]);
-  }
+    int i = 0;
+    while (cmd[i] != '\0'){
+      ioctl(0, TIOCSTI, &cmd[i++]);
+    }
 }
 
 int main(int argc, char ** argv){
-    char buffer[1024] = "\0";
+    char buffer[PATH_MAX] = "\0";
     char target;
 
     if(argc == 3)
     {
-            strcat(buffer,TEMPLATE);
-            target = (char) argv[1][0];
-            buffer[7] = target;
-            buffer[8] = '=';
-            buffer[9] = '\0';
+        strcat(buffer,TEMPLATE);
+        target = (char) argv[1][0];
+        buffer[7] = target;
+        buffer[8] = '=';
+        buffer[9] = '\0';
+        if(strcmp(argv[2],".")==0)
+        {
+            char cwd[PATH_MAX];
+            if(getcwd(cwd,PATH_MAX) != NULL)
+            {
+              strcat(buffer,cwd);
+            } else {printf("sd : error fetching the current directory");return 1;}
+        }
+        else
+        {
             strcat(buffer,argv[2]);
-            strcat(buffer,"\r");
-            inject_shell(buffer);
+        }
+        strcat(buffer,"\r");
+        inject_shell(buffer);
     } else {
         if(argc == 2)
         {
